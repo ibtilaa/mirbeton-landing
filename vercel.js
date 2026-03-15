f733388fa1dd46ce71d0664a13e7f2969f1d1810
Amriddin <129955505+ibtilaa@users.noreply.github.com>
2026-03-15 05:07:18 +0500

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,20 @@
+{
+  "rewrites": [
+    {
+      "source": "/api/webhook",
+      "destination": "/api/index.py"
+    },
+    {
+      "source": "/api/order",
+      "destination": "/api/index.py"
+    },
+    {
+      "source": "/api/prices",
+      "destination": "/api/index.py"
+    },
+    {
+      "source": "/api/driver-event",
+      "destination": "/api/index.py"
+    }
+  ]
+}
